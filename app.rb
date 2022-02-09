bbdf00010df4e238105b6abc1703dfdc53c5bbbf
David <dave_carroll82@hotmail.com>
2022-02-09 16:51:32 +0000

    section 10 complete

--- app.rb
+++ app.rb
@@ -12,9 +12,19 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
+    # p ENV
     @bookmarks = Bookmark.all
-    erb :index
+    erb :'bookmarks/index'
   end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+  redirect '/bookmarks'
+end
 
   run! if app_file == $0
 end
--- spec/features/bookmark_spec.rb
+++ spec/features/bookmark_spec.rb
@@ -1,9 +1,18 @@
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
-    visit('/bookmarks')
+    # connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    # # Add the test data
+    # connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    # connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    # connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    # visit('/bookmarks')
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
+
+    # expect(page).to have_content "http://www.makersacademy.com"
+    # expect(page).to have_content "http://www.destroyallsoftware.com"
+    # expect(page).to have_content "http://www.google.com"
   end
 end
